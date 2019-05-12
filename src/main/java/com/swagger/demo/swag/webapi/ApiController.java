d9fd5e3779e9ed7324a0bd58b7c0b5fc1cc9473e
Swati Sharma <swatiinfo86@gmail.com>
2019-05-12 11:40:36 -0400

    adding project

---
+++ src/main/java/com/swagger/demo/swag/webapi/ApiController.java
@@ -0,0 +1,44 @@
+package com.swagger.demo.swag.webapi;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponse;
+
+@RestController
+public class ApiController {
+	 
+	@ApiOperation(value = "View a list of available apartments")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved the list of apartments")
+    })
+	@RequestMapping(value = "/getListApi", method = RequestMethod.GET)
+	public ResponseEntity custom() {
+	    return new ResponseEntity("hey list api", HttpStatus.OK);
+	}
+	
+	@ApiOperation(value = "View a the specified apartment")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved the apartment")
+    })
+	@GetMapping("/getOneApi")
+    public ResponseEntity timeConsumingAPI(@RequestParam(value = "delay", defaultValue = "0") Integer delay) throws InterruptedException {
+        if(delay == 0) {
+            Random random = new Random();
+            delay = random.nextInt(10);
+        }
+
+        TimeUnit.SECONDS.sleep(delay);
+        return new ResponseEntity("Result", HttpStatus.OK);
+    }
+}
---
+++ src/test/java/com/swagger/demo/swag/SwagApplicationTests.java
@@ -0,0 +1,16 @@
+package com.swagger.demo.swag;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SwagApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
